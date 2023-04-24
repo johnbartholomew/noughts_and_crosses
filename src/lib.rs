1a97ec9904fffc4bf3489587ac5b83d5ac295177
John Bartholomew <jpa.bartholomew@gmail.com>
2023-04-24 23:41:22 +0100

    Improve code structure for safety, add tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,57 @@
-/// Represents the final status of a game
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub enum Status {
-    Loss,
-    Draw,
-    Win,
-}
+//! A solver for the game Noughts and Crosses, also known as Tic-Tac-Toe.
 
-// Internally we use a signed int so that we can negate it to get the 'other' player's win state.
-const LOSS: i32 = -1;
-const WIN: i32 = 1;
-const DRAW: i32 = 0;
+mod board {
+    // STYLE NOTE:
+    // A (sub-)module for 'board' means that visibility can be controlled,
+    // so that a clean and safe interface can be exposed for 'solve' to use.
 
-/// Represents the noughts and crosses board
-pub struct Board {
-    player: u16,
-    opponent: u16,
-}
+    /// Represents the noughts and crosses board.
+    #[derive(Default, Clone, PartialEq, Eq)]
+    pub struct Board {
+        player: u16,
+        opponent: u16,
+    }
 
-impl Board {
-    /// The binary representation of a full board
-    const FULL: u16 = 0b111111111;
+    impl std::fmt::Display for Board {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let play_count = (self.player | self.opponent).count_ones();
+            let mut buf = *b"   \n   \n   ";
+            let (ex, oh) = if play_count & 1 == 1 {
+                (self.opponent, self.player)
+            } else {
+                (self.player, self.opponent)
+            };
+            for i in 0..=9 {
+                let row = i / 3;
+                let row = &mut buf[(4 * row)..];
+                let b = 1u16 << i;
+                if ex & b == b {
+                    row[i] = b'X';
+                } else if oh & b == b {
+                    row[i] = b'O';
+                }
+            }
+            // SAFETY: The string is UTF-8 (actually ASCII) by construction since it only uses ASCII characters.
+            f.write_str(std::str::from_utf8(&buf).unwrap())
+        }
+    }
+
+    impl std::fmt::Debug for Board {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            if f.alternate() {
+                write!(
+                    f,
+                    "Board{{ player: {:#09b}, opponent: {:#09b} }}",
+                    self.player, self.opponent
+                )
+            } else {
+                write!(f, "Board({:#09b}, {:#09b})", self.player, self.opponent)
+            }
+        }
+    }
 
     /// The binary representation of the lines of three on the board
-    const LINES: [u16; 8] = [
+    const LINE_MASKS: [u16; 8] = [
         0b000000111,
         0b000111000,
         0b111000000,
@@ -33,72 +62,259 @@ impl Board {
         0b001010100,
     ];
 
-    /// Constructs a new instances representing an empty board
-    pub fn new() -> Self {
-        Self {
-            player: 0,
-            opponent: 0,
-        }
-    }
+    // STYLE NOTE:
+    // When I have an embedded block of constant/static data I like to validate
+    // properties of it with one or more tests. This helps ensure I haven't made
+    // a silly copy-paste mistake that might not be easy to spot in the data itself.
+    #[test]
+    fn test_line_masks() {
+        // Some properties of the set of lines of 3.
+        let lines_sorted = {
+            let mut ls = LINE_MASKS.clone();
+            ls.sort_unstable();
+            ls
+        };
 
-    /// Returns whether the current player has lost
-    fn has_lost(&self) -> bool {
-        Self::LINES.iter().any(|&line| self.opponent & line == line)
-    }
-
-    fn has_won(&self) -> bool {
-        Self {
-            player: self.opponent,
-            opponent: self.player,
-        }
-        .has_lost()
-    }
-
-    fn combined(&self) -> u16 {
-        self.player | self.opponent
-    }
-
-    fn remaining_bits(&self) -> u16 {
-        Self::FULL & !self.combined()
-    }
-
-    fn with_move_bit(&self, position: u16) -> Self {
-        debug_assert!(
-            position & Self::FULL == position,
-            "not a valid board bitmap"
+        assert!(
+            lines_sorted.windows(2).all(|w| w[0] < w[1]),
+            "Valid lines should be distinct."
         );
-        debug_assert!(
-            position.count_ones() == 1,
-            "not a valid single-move board bitmap"
+        assert!(
+            lines_sorted.into_iter().all(|x| x.count_ones() == 3),
+            "Valid lines should each have exactly 3 bits set."
         );
-        debug_assert!(self.combined() & position == 0, "position is already taken");
+    }
 
-        Self {
-            player: self.opponent,
-            opponent: self.player | position,
+    // STYLE NOTE:
+    // I extracted this out of 'board' because I wanted to be able to use it
+    // easily on either 'player' or 'opponent'. It doesn't need 'both' halves
+    // of the board and pulling it out of the Board implementation makes that
+    // a little clearer. It can be tested separately as well.
+    fn has_won(places: u16) -> bool {
+        LINE_MASKS.iter().any(|&line| places & line == line)
+    }
+
+    #[test]
+    fn test_has_won() {
+        // Spot check has_won against some obvious cases.
+        assert!(!has_won(0u16));
+        assert!(has_won(0b111111111));
+        assert!(has_won(0b111000000));
+        assert!(has_won(0b000000111));
+        assert!(!has_won(0b101000101));
+        assert!(has_won(0b101010101));
+    }
+
+    // STYLE NOTE:
+    // This is a pretty simple bit twiddling hack; putting it into a function
+    // allows it to be tested easily (and since it's only u16 it can be tested
+    // exhaustively very cheaply).
+    const fn lowest_bit16(bits: u16) -> u16 {
+        if bits != 0 {
+            bits & !(bits - 1)
+        } else {
+            0
         }
+    }
+
+    // STYLE NOTE:
+    // When the state space is small I like exhaustive tests, as long as it's
+    // actually _testing_ something. In this case I have an "alternative" implementation
+    // of lowest_bit16, using the trailing_zeros() method (standard library method on u16).
+    #[test]
+    fn test_lowest_bit16_exhaustive() {
+        assert_eq!(lowest_bit16(0), 0);
+        for n in 1..=u16::MAX {
+            let lo = lowest_bit16(n);
+            assert_eq!(lo, 1u16 << n.trailing_zeros());
+        }
+    }
+
+    /// An Iterator of valid next moves constructed from a board.
+    pub struct Moves {
+        base: Board,
+        remain: u16,
+    }
+
+    impl std::iter::Iterator for Moves {
+        type Item = Board;
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.remain != 0 {
+                let b = lowest_bit16(self.remain);
+                self.remain &= !b;
+                Some(self.base.with_move(b))
+            } else {
+                None
+            }
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct InvalidBoard(&'static str);
+
+    impl std::fmt::Display for InvalidBoard {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_str("invalid board: ")?;
+            f.write_str(self.0)
+        }
+    }
+
+    impl Board {
+        const FULL: u16 = 0b111111111;
+
+        /// Constructs an empty board.
+        pub fn new() -> Self {
+            Self::default()
+        }
+
+        /// Constructs a new board with the specified play pattern.
+        pub fn from_bits(player: u16, opponent: u16) -> Result<Self, InvalidBoard> {
+            if player & Self::FULL != player {
+                return Err(InvalidBoard("player has excess bits set"));
+            }
+            if opponent & Self::FULL != opponent {
+                return Err(InvalidBoard("opponent has excess bits set"));
+            }
+            if player ^ opponent != player | opponent {
+                return Err(InvalidBoard(
+                    "player and opponent have both played the same cell",
+                ));
+            }
+            let player_turns = player.count_ones();
+            let opponent_turns = opponent.count_ones();
+            if opponent_turns > player_turns + 1 {
+                return Err(InvalidBoard("opponent has had too many turns"));
+            }
+            if player_turns > opponent_turns {
+                return Err(InvalidBoard("player has had too many turns"));
+            }
+            Ok(Board { player, opponent })
+        }
+
+        /// Returns true if the opponent won (we lost).
+        pub fn has_lost(&self) -> bool {
+            has_won(self.opponent)
+        }
+
+        /// Returns true if we won (opponent lost).
+        pub fn has_won(&self) -> bool {
+            has_won(self.player)
+        }
+
+        /// Returns an iterator of valid next board positions.
+        /// Note that the resulting boards will be "flipped": opponent becomes player, player becomes opponent.
+        pub fn moves(&self) -> Moves {
+            let remain = Self::FULL & !(self.player | self.opponent);
+            Moves {
+                base: self.clone(),
+                remain,
+            }
+        }
+
+        // Private because it relies on only accepting 'valid' next moves.
+        fn with_move(&self, position: u16) -> Self {
+            // with_move is only called from the Moves iterator, and Moves is constructed
+            // so that it only picks valid (open) cells to try, so position should always
+            // be valid (as checked by this debug_assert) by construction.
+            // If with_move was public then the argument should be checked by assert!()
+            // or should be handled as a clean error (ie, with a Result type).
+            debug_assert!(
+                position > 0 && position <= 0b100000000 && position.is_power_of_two(),
+                "position is invalid"
+            );
+            debug_assert!(
+                position & (self.player | self.opponent) == 0,
+                "position is already taken"
+            );
+
+            let ret = Self {
+                player: self.opponent,
+                opponent: self.player | position,
+            };
+
+            // SELF-CHECK: player and opponent don't have any overlapping cells.
+            debug_assert!(
+                (self.player ^ self.opponent) == (self.player | self.opponent),
+                "player and opponent have both played in the same cell"
+            );
+
+            // SELF-CHECK: from_bits() could construct the same board.
+            debug_assert!(Board::from_bits(self.player, self.opponent).unwrap() == *self);
+
+            ret
+        }
+    }
+
+    #[test]
+    fn test_moves_from_empty() {
+        let mut boards: Vec<_> = Board::new().moves().map(|b| b.opponent).collect();
+        boards.sort_unstable();
+        let want: Vec<_> = (0..9).map(|x| 1u16 << x).collect();
+        assert_eq!(want.len(), 9);
+        assert_eq!(boards, want);
+    }
+
+    #[test]
+    fn test_moves_from_full() {
+        let full = Board {
+            player: 0b101010101,
+            opponent: 0b010101010,
+        };
+        assert!(full.moves().next().is_none());
+    }
+
+    #[test]
+    fn test_moves() {
+        let bb = |player, opponent| Board { player, opponent };
+        assert_eq!(bb(0b110000000, 0b001001000).moves().count(), 5);
+
+        // I would spot-check more, but I'm lazy.
+    }
+
+    #[test]
+    fn test_from_bits() {
+        // Things that are not ok:
+        let not_ok = |p, o| assert!(Board::from_bits(p, o).is_err());
+        not_ok(1, 1); // Both players play cell 0.
+        not_ok(1u16 << 6, 1u16 << 6); // Both players play cell 6.
+        not_ok(1u16 << 9, 0); // Player plays out-of-range.
+        not_ok(0, 1u16 << 9); // Opponent plays out-of-range.
+        not_ok(1u16 << 15, 0); // Player plays out-of-range.
+        not_ok(0, 1u16 << 15); // Opponent plays out-of-range.
+        not_ok(1, 0); // Player has had a turn but opponent has not.
+        not_ok(0b001000011, 0b000011000); // Opponent has had 4 turns, player has had 5.
+
+        // Things that are ok:
+        let is_ok = |p, o| assert!(Board::from_bits(p, o).is_ok());
+        is_ok(0, 1); // Opponent has had a turn but player has not.
+        is_ok(0b000011000, 0b001000011); // Opponent has had 3 turns, player has had 2.
+        is_ok(0b010011000, 0b001000011); // Opponent has had 3 turns, player has had 3.
+        is_ok(0, 0); // Both players have had 0 turns.
+        assert_eq!(Board::from_bits(0, 0).unwrap(), Board::new());
     }
 }
 
-fn solve_inner(board: Board) -> (i32, usize) {
+pub use board::Board;
+
+type StatusInt = i32;
+
+// Internally we use a signed int so that we can negate it to get the 'other' player's win state.
+const LOSS: StatusInt = -1;
+const WIN: StatusInt = 1;
+const DRAW: StatusInt = 0;
+
+fn solve_inner(board: Board) -> (StatusInt, usize) {
     debug_assert!(
         !board.has_won(),
-        "We already won so we should not be trying more moves."
+        "player already won; should have bailed out earlier!"
     );
     if board.has_lost() {
         return (LOSS, 1);
     }
 
-    let mut bits = board.remaining_bits();
-    if bits == 0 {
-        return (DRAW, 1);
-    }
     let mut best_result = -1;
     let mut games = 0;
-    while bits != 0 {
-        let low_bit = bits & !(bits - 1);
-        bits &= !low_bit;
-        let opponent_board = board.with_move_bit(low_bit);
+    for opponent_board in board.moves() {
         let (result, n) = solve_inner(opponent_board);
         games += n;
         // Negate the opponent's result to get our result.
@@ -107,11 +323,23 @@ fn solve_inner(board: Board) -> (i32, usize) {
             break;
         }
     }
-    (best_result, games)
+    if games == 0 {
+        (DRAW, 1)
+    } else {
+        (best_result, games)
+    }
 }
 
-/// Returns whether the game is a win, draw, or loss for the current player
-/// starting from the specified board position
+/// Represents the final status of a game
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub enum Status {
+    Loss,
+    Draw,
+    Win,
+}
+
+/// Returns whether the game is a win, draw, or loss for the current player starting from the
+/// specified board position, and the count of boards examined not including the input board.
 pub fn solve(board: Board) -> (Status, usize) {
     let (result, n) = solve_inner(board);
     let result = match result {
@@ -123,36 +351,7 @@ pub fn solve(board: Board) -> (Status, usize) {
     (result, n)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn has_lost() {
-        let mut board = Board::new();
-
-        for position in 0..=6 {
-            assert!(!board.has_lost());
-            board = board.with_move_bit(1u16 << position);
-        }
-
-        // At this point the board is:
-        //
-        // xox
-        // oxo
-        // x..
-        assert!(board.has_lost());
-    }
-
-    #[test]
-    fn has_moves() {
-        let mut board = Board::new();
-
-        for position in 0..=8 {
-            assert!(board.remaining_bits() != 0);
-            board = board.with_move_bit(1u16 << position);
-        }
-
-        assert!(board.remaining_bits() == 0);
-    }
+#[test]
+fn test_solve_from_empty() {
+    assert_eq!(solve(Board::new()), (Status::Draw, 38856));
 }
